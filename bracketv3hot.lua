eb8349a22a4d330a99080f4a068cdee6dd168466
Aknoks <60153476+Aknoks@users.noreply.github.com>
2022-04-12 04:00:23 +0200

    Update bracketv3hot.lua

--- bracketv3hot.lua
+++ bracketv3hot.lua
@@ -57,7 +57,8 @@ function Library:CreateWindow(Config, Parent)
 	Screen.Name =  HttpService:GenerateGUID(false)
 	Screen.Parent = Parent
 	Topbar.WindowName.Text = Config.WindowName
-	Topbar.LibraryName.Text = "ntm"
+	Topbar.LibraryName.Visible = true
+	Topbar.LibraryName.Text = Config.Version
 
 	MakeDraggable(Topbar,Main)
 	local function CloseAll()
